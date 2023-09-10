121dc52ad741d195c0de6f8949d0a71a7ee7ad45
AdvanceCodeMarshall <mgmbcs.santosh@gmail.com>
2023-09-10 15:51:17 +0530

    fixed docker command

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String project,String imageTag, String hubUser){
     sh """
      docker image build -t ${hubUser}:${project} .
-     docekr image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
-     docekr image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
