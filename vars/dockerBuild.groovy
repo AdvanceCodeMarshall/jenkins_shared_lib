fcbc020089862b2ac55b14e6e40812644f227bec
AdvanceCodeMarshall <mgmbcs.santosh@gmail.com>
2023-09-28 23:21:05 +0530

    Changed docker build configuration for ECR

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,15 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
     
+//     sh """
+//      docker image build -t ${hubUser}/${project} . 
+//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// }
+
+def call(String awsAccountId, String region, String ecrRepoName){
     sh """
-     docker image build -t ${hubUser}/${project} . 
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+        docker build -t ${ecrRepoName} .
+        docker tag ${ecrRepoName}:latest ${awsAccountId}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:latest
     """
 }
