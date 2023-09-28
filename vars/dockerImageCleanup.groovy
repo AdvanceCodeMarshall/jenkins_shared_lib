1227a508fb15446dc301c80bf80de377348582a6
AdvanceCodeMarshall <mgmbcs.santosh@gmail.com>
2023-09-28 23:38:03 +0530

    changed ecr configuration for cleanup

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,7 +1,13 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
     
+//     sh """
+//      docker rmi ${hubUser}/${project}:${ImageTag}
+//      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// }
+
+def call(String awsAccountId, String region, String ecrRepoName){
     sh """
-     docker rmi ${hubUser}/${project}:${ImageTag}
-     docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+        docker rmi ${ecrRepoName}:latest ${awsAccountId}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:latest
     """
 }
