27dfd652b8ae2d86304367bcfb4d31a352b0208a
AdvanceCodeMarshall <mgmbcs.santosh@gmail.com>
2023-09-29 19:59:42 +0530

    fixed

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -14,9 +14,9 @@ def call(String aws_account_id, String region, String ecr_repoName){
     
     withCredentials([[
       $class: 'AmazonWebServicesCredentialsBinding',
-      credentialsId: 'aws';
-      accessKeyVariable: 'AWS_ACCESS_KEY_ID'
-      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+      credentialsId: 'aws',
+      accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY',
         sh """
           aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
           docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
