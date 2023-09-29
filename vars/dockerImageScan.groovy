af9cdd33a7efa6b49f597d662a8bc8cd6746fe9f
AdvanceCodeMarshall <mgmbcs.santosh@gmail.com>
2023-09-29 17:53:44 +0530

    ECR scripts enabled

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,14 +1,14 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
     
-    sh """
-     trivy image ${hubUser}/${project}:latest > scan.txt
-     cat scan.txt
-    """
-}
-
-// def call(String aws_accout_id, String region, String ecr_repoName){
 //     sh """
-//         trivy image ${aws_accout_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
-//         cat scan.txt
+//      trivy image ${hubUser}/${project}:latest > scan.txt
+//      cat scan.txt
 //     """
 // }
+
+def call(String aws_accout_id, String region, String ecr_repoName){
+    sh """
+        trivy image ${aws_accout_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+        cat scan.txt
+    """
+}
