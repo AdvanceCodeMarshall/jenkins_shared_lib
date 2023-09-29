36bfd227cc4f0d7d66b453546b8da3c8da8e66e9
AdvanceCodeMarshall <mgmbcs.santosh@gmail.com>
2023-09-29 17:38:19 +0530

    docker rmi fixed

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,6 +1,7 @@
 def call(String project, String ImageTag, String hubUser){
+    //docker rmi ${hubUser}/${project}:${ImageTag}
+
     sh """
-     docker rmi ${hubUser}/${project}:${ImageTag}
      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
